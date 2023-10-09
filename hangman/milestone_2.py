ae28147c33114336da3ba786c396506f851454ed
selvatica-36 <silvia.h.aragon@gmail.com>
2023-10-09 18:59:34 +0100

    Checks if input is a single letter.

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -7,3 +7,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("Please, enter a letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
