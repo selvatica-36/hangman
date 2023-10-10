8fd463bc73392e953120fdf3c49bbb30d39773d3
selvatica-36 <silvia.h.aragon@gmail.com>
2023-10-10 20:49:47 +0100

    Adds comments

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,13 +1,18 @@
 import random
 
+# Define the list of possible words:
 word_list = ["plum", "apple", "banana", "mango", "nectarine"]
 print(word_list)
 
+# Computer chooses a random word from the list, using the random module
 word = random.choice(word_list)
 print(word)
 
+# Program asks for user input
 guess = input("Please, enter a letter: ")
 
+
+# Checks whether user input is valid: needs to be a single letter in the alphabet
 if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
