11e33899781208481ca21a17f45b385f2cafaa88
selvatica-36 <silvia.h.aragon@gmail.com>
2023-10-10 20:49:36 +0100

    Organises code into functions

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,19 +1,29 @@
-# Comment here
 import random
 
+# Define the list of possible words:
 word_list = ["plum", "apple", "banana", "mango", "nectarine"]
 word = random.choice(word_list)
 
 
-while True:
-    guess = input("Please, guess a letter: ")
-    if len(guess) == 1 and guess.isalpha():
-        break
+# Create a function that checks user's guess is in the chosen word
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! The letter {guess} is in the word.")
     else:
-        print("Invalid guess. Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
 
-if guess in word:
-    print(f"Good guess! The letter {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+# Create a function that asks for input, checks it is valid and feeds it to check_guess() function
+def ask_for_input():
+    while True:
+        guess = input("Please, guess a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid guess. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+
+# Call the function to check if code works
+ask_for_input()
