82ae3b277f342b8733d733955f5f5b6cd78e3ab6
selvatica-36 <silvia.h.aragon@gmail.com>
2023-10-10 21:12:05 +0100

    Adds docstrings to functions

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -7,6 +7,7 @@ word = random.choice(word_list)
 
 # Create a function that checks user's guess is in the chosen word
 def check_guess(guess):
+    ''' This function checks if the user's guess is in the word '''
     guess = guess.lower()
     if guess in word:
         print(f"Good guess! The letter {guess} is in the word.")
@@ -16,6 +17,11 @@ def check_guess(guess):
 
 # Create a function that asks for input, checks it is valid and feeds it to check_guess() function
 def ask_for_input():
+    ''' 
+    This function asks user to guess a letter from the chosen word.
+    It also checks that user input is valid: a single alphabetical character.
+    If input is invalid, it prints an error message 
+    '''
     while True:
         guess = input("Please, guess a letter: ")
         if len(guess) == 1 and guess.isalpha():
