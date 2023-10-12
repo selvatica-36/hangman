98028435d7e2ae1d8affd6deccce79f15ded61e6
selvatica-36 <silvia.h.aragon@gmail.com>
2023-10-12 20:46:01 +0100

    Adds class constructor.

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -3,8 +3,10 @@ import random
 class Hangman:
     def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list)
-        self.word_guessed = len(self.word) * ['_'] #TODO: revise this here
-        self.num_letters = len(set(word))
+        self.word_guessed = len(self.word) * ['_'] #TODO: revise if this is correct
+        self.num_letters = len(set(self.word))
         self.word_list = word_list
         self.num_lives = num_lives
         self.list_of_guesses = []
+
+    
