17d82582f493a14f0527c06720fb50c8cd0f39d0
selvatica-36 <silvia.h.aragon@gmail.com>
2023-10-12 21:29:36 +0100

    Updates check_guess method.

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -3,19 +3,25 @@ import random
 class Hangman:
     def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list)
-        self.word_guessed = len(self.word) * ['_'] #TODO: revise if this is correct
+        self.word_guessed = len(self.word) * ['_'] 
         self.num_letters = len(set(self.word))
         self.word_list = word_list
         self.num_lives = num_lives
-        self.list_of_guesses = ['a','b']
+        self.list_of_guesses = []
 
 
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            for letter in self.word:
+                if letter == guess:
+                    self.word_guessed.index(letter) == letter
+            self.num_letters -= 1
         else:
+            self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word. Try again.")
+            print(f"You have {num_lives} lives left.")
 
 
     def ask_for_input(self):
@@ -29,6 +35,7 @@ class Hangman:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
         
+
 
 word_list_1 = ["plum", "apple", "banana", "mango", "nectarine"]
 game_test = Hangman(word_list_1)
